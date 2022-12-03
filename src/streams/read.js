e9387edc7405750348ec5ffe34336c406e08f814
Jardozzz <jardozzz@gmail.com>
2022-12-03 14:18:15 +0300

    fix:fixed bug

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,12 +1,14 @@
 import * as path from 'path'
 import * as fs from 'fs'
 import { fileURLToPath } from 'url'
+import { Stream } from 'stream';
 const read = async () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
     let p=path.resolve(__dirname,'files/fileToRead.txt')
     const input=fs.createReadStream(p)
-   input1.pipe(process.stdout)
+    const input1=new Stream.Readable(p)
+   input.pipe(process.stdout)
 
 };
 
